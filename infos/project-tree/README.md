325945a492cc23023107b83859fe19d4fe4c808b
ON00dev <vicguioliveira2005@gmail.com>
2024-12-12 13:12:55 -0300

    v0.1.1

--- infos/project-tree/README.md
+++ infos/project-tree/README.md
@@ -1,22 +1,28 @@
 
 ```
 Fivhit2d-Arena
+├─ add_personagem.html
 ├─ index.html
 ├─ infos
-│  ├─ créditos.txt
+│  ├─ creditos.txt
 │  ├─ Logo.png
 │  ├─ project-tree
 │  │  └─ README.md
 │  └─ versão.txt
 ├─ jogando.html
 ├─ jogo.html
-├─ personagens.json
-└─ persons
-   ├─ inie
-   │  ├─ codigo.xml
-   │  └─ inie.png
-   └─ inie_reverse
-      ├─ codigo.xml
-      └─ inie_reverse.png
+├─ persons
+│  ├─ inie
+│  │  ├─ codigo.xml
+│  │  └─ inie.png
+│  └─ inie_reverse
+│     ├─ codigo.xml
+│     └─ inie_reverse.png
+└─ static
+   ├─ js
+   │  ├─ add_personagem.js
+   │  ├─ jogando.js
+   │  └─ jogo.js
+   └─ personagens.json
 
 ```
---
+++ static/js/jogo.js
@@ -0,0 +1,65 @@
+let jogadorAtual = 1;
+let p1Selecionado = null;
+let p2Selecionado = null;
+
+localStorage.clear();
+
+fetch('/static/personagens.json')
+    .then(response => response.json())
+    .then(personagens => {
+        const grid = document.getElementById('grid');
+        
+        personagens.forEach(char => {
+            const box = document.createElement('div');
+            box.className = 'char-box';
+            
+            const img = document.createElement('img');
+            img.src = char.image;
+            img.alt = char.name;
+            img.className = 'char-img'; 
+            box.appendChild(img);
+
+            
+            const nameDiv = document.createElement('div');
+            nameDiv.className = 'char-name';
+            nameDiv.textContent = char.name;
+            box.appendChild(nameDiv);
+
+            box.dataset.charId = char.id;
+            box.onclick = (event) => selecionarPersonagem(event, char);
+            grid.appendChild(box);
+        });
+    });
+
+function selecionarPersonagem(event, char) {
+    const box = event.currentTarget;
+
+    if (jogadorAtual === 1) {
+        if (p1Selecionado) {
+            p1Selecionado.classList.remove('selecionado-p1');
+        }
+        box.classList.add('selecionado-p1');
+        p1Selecionado = box;
+
+        localStorage.setItem('char1', JSON.stringify({ id: char.id, name: char.name, status: "1" }));
+
+        jogadorAtual = 2;
+        document.getElementById('status').textContent = 'Jogador 2, selecione seu personagem';
+    } else {
+        if (p2Selecionado) {
+            p2Selecionado.classList.remove('selecionado-p2');
+        }
+        box.classList.add('selecionado-p2');
+        p2Selecionado = box;
+
+        localStorage.setItem('char2', JSON.stringify({ id: char.id, name: char.name, status: "2" }));
+
+        document.getElementById('jogar').style.display = 'block';
+    }
+}
+
+document.getElementById('jogar').onclick = () => {
+    if (p1Selecionado && p2Selecionado) {
+        window.location.href = 'jogando.html';
+    }
+};
